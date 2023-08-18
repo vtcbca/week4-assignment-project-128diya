e7c7a7e9422d60046832ef4200f104dd737f64b6
128diya <139377694+128diya@users.noreply.github.com>
2023-08-18 14:22:20 +0530

    Update myfile1.py

--- Week5/myfile1.py
+++ Week5/myfile1.py
@@ -1,49 +1,15 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[7]:
-
-
 import csv
 
-
-# In[8]:
-
-
 f=open('c:\\sqlite3\csv\myfile.txt','r')
-
-
-# In[9]:
-
-
 d=f.readline()
-
-
-# In[10]:
-
-
 print(d)
 
-
-# In[11]:
-
-
 d1=f.readlines()
-
-
-# In[12]:
-
-
 print(d1)
-
-
-# In[13]:
-
-
 f.close()
 
 
-# In[ ]:
+
 
 
 
